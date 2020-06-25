fc4479d26fb74b6936d5ab446eda148d1fa0cf03
GemmaMorales <gemmaamorales@gmail.com>
2020-06-25 21:29:12 +0000

    updated models.py

--- src/models.py
+++ src/models.py
@@ -16,32 +16,71 @@ db = SQLAlchemy()
 #             "email": self.email
 #         }
 
-class User(Base):
-    __tablename__ = "user"
-    user_id = Column(Integer, primary_key=True)
-    password = Column(String)
-    name = Column(String)
-    email_address= Column(String)
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    qb_id = db.Column(db.Integer, unique=True)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
 
-class Client(Base):
-    __tablename__ = "client"
-    client_id = Column(Integer, primary_key=True)
-    company_name = Column(String)
-    bookkeeper = Column(Integer, ForeignKey("user.user_id"))
-    email_address = Column(String)
+    clients = db.relationship ("Clients")
+    special_codes = db.relationship ("Special_Codes")
 
-class Transaction(Base):
-    __tablename__ = "transaction"
-    transaction_id = Column(Integer, primary_key=True)
-    client = Column(Integer, ForeignKey("client.client_id"))
-    date = Column(Date)
-    amount = Column(Float)
-    transaction_type = Column(Enum("expense","revenue"))
-    name = Column(String)
-    GL_account = Column(Integer)
+    def serialize(self):
+        return {
+            "username": self.user_id,
+            "password": self.password,
+            "name": self.name,
+            "email": self.email
+        }
 
-class Unknown(Base):
-    __tablename__ = "unknown"
-    unknown_id = Column(Integer, primary_key=True)
-    transaction = Column(Integer, ForeignKey(transaction.transaction_id))
-    missing_entity = Column(Enum("vendor_name","customer_name","GL_acct"))
+class Client(db.Model):
+    client_id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String)
+    user_id = db.Column(db.Integer, ForeignKey("User.user_id"))
+    email = db.Column(db.String, unique=True)
+
+    transactions = db.relationship ("Transactions")
+
+    def serialize(self):
+        return {
+            "client_id": self.client_id,
+            "company_name": self.company_name,
+            "user_id": self.user_id,
+            "email": self.email
+        }
+
+class Transaction(db.Model):
+    transaction_id = db.Column(db.Integer, primary_key=True)
+    client = db.Column(db.Integer, ForeignKey("client.client_id"))
+    date = db.Column(db.Date)
+    amount = db.Column(db.Float)
+    transaction_type = db.Column(db.Enum("expense","revenue"))
+    vendor_qb_id = db.Column(db.String)
+    customer_qb_id = db.Column(db.String)
+    GL_acct = db.Column(db.Integer)
+
+    def serialize(self):
+        return {
+            "transaction_id": self.transaction_id,
+            "client": self.client,
+            "date": self.namedate,
+            "amount": self.amount,
+            "transaction_type": self.transaction_type,
+            "vendor_qb_id": self.vendor_qb_id,
+            "customer_qb_id": self.customer_qb_id,
+            "GL_acct": self.GL_acct
+        }
+
+class Special_Codes(db.Model):
+    code_type = db.Column(db.Enum("vendor_name","customer_name","GL_acct"))
+    user_id = db.Column(db.Integer, ForeignKey(User.user_id))
+    code = db.Column(db.String)
+    
+
+    def serialize(self):
+        return {
+            "code_type": self.code_type,
+            "user_id": self.user_id,
+            "code": self.code
+        }
